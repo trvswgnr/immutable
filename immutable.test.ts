55e6a9c3c0a238ee648d92c2ee5201a1ed4ce833
Travis Wagner <travis.wagner@umusic.com>
2024-05-22 21:54:42 -0400

    tests

--- immutable.test.ts
+++ immutable.test.ts
@@ -1,106 +1,65 @@
 import { describe, it, expect } from "bun:test";
-import { Immutable } from "./immutable.ts";
+import { immutable } from "./immutable";
 
-describe("Immutable", () => {
-    it("should return the same value for primitive types", () => {
-        expect(Immutable(null)).toBe(null);
-        expect(Immutable(undefined)).toBe(undefined);
-        expect(Immutable(true)).toBe(true);
-        expect(Immutable(123)).toBe(123);
-        expect(Immutable("test")).toBe("test");
-        expect(Immutable(Symbol.for("test"))).toBe(Symbol.for("test"));
-        expect(Immutable(BigInt(123))).toBe(BigInt(123));
+describe("immutable", () => {
+    it("should create an immutable array", () => {
+        const List = immutable(Array);
+        expect(() => List.from([1, 2, 3])).not.toThrow();
+        const li = List.from([1, 2, 3]);
+        expect(() => li.push(4)).toThrowError();
+        expect(() => (li[0] = 5)).toThrowError();
+        expect(li.length).toBe(3);
     });
 
-    it("should return an immutable object for non-primitive types", () => {
-        const obj = { a: 1, b: { c: 2 } };
-        const immutableObj = Immutable(obj);
-
-        expect(immutableObj).not.toBe(obj);
-        expect(immutableObj.a).toBe(1);
-        expect(immutableObj.b).not.toBe(obj.b);
-        expect(immutableObj.b.c).toBe(2);
+    it("should create an immutable Map", () => {
+        const ImmutableMap = immutable(Map);
+        expect(() => new ImmutableMap([["key1", "value1"]])).not.toThrow();
+        const map = new ImmutableMap([["key1", "value1"]]);
+        expect(() => map.set("key2", "value2")).toThrowError();
+        expect(map.size).toBe(1);
     });
 
-    it("should not allow properties to be set", () => {
-        const obj = { a: 1 };
-        const immutableObj = Immutable(obj);
-
-        expect(() => {
-            // @ts-expect-error - assign to a read-only property should error
-            immutableObj.a = 2;
-        }).toThrowError(new Error("Cannot assign to 'a' because it is a read-only property."));
+    it("should create an immutable Set", () => {
+        const ImmutableSet = immutable(Set);
+        expect(() => new ImmutableSet([1, 2, 3])).not.toThrow();
+        const set = new ImmutableSet([1, 2, 3]);
+        expect(() => set.add(4)).toThrowError();
+        expect(set.size).toBe(3);
     });
 
-    it("should return bound functions", () => {
-        const obj = {
-            a: 1,
-            b() {
-                return this.a;
-            },
-        };
-        const immutableObj = Immutable(obj);
-
-        expect(immutableObj.b()).toBe(1);
+    it("should create an immutable Date", () => {
+        const ImmutableDate = immutable(Date);
+        expect(() => new ImmutableDate("2020-01-01")).not.toThrow();
+        const date = new ImmutableDate("2020-01-01");
+        expect(() => date.setFullYear(2021)).toThrowError();
+        expect(date.getFullYear()).toBe(2020);
     });
 
-    it("should return immutable objects within objects", () => {
-        const obj = {
-            a: 1,
-            b: {
-                c: 2,
-            },
-        };
-        const immutableObj = Immutable(obj);
-
-        expect(immutableObj.b).not.toBe(obj.b);
-        expect(immutableObj.b.c).toBe(2);
+    it("should instantiate Array without error", () => {
+        const List = immutable(Array);
+        expect(() => new List()).not.toThrow();
     });
 
-    it("should return immutable arrays", () => {
-        const arr = [1, 2, 3];
-        const immutableArr = Immutable(arr);
-
-        expect(immutableArr).not.toBe(arr);
-        expect([...immutableArr]).toEqual(arr);
-        expect(() => {
-            // @ts-expect-error - assign to a read-only property should error
-            immutableArr[0] = 4;
-        }).toThrowError(new Error("Cannot assign to '0' because it is a read-only property."));
-        expect(immutableArr[0]).toBe(1);
-        expect(immutableArr[1]).toBe(2);
-        expect(immutableArr[2]).toBe(3);
-        expect(immutableArr.length).toBe(3);
+    it("should instantiate Map without error", () => {
+        const ImmutableMap = immutable(Map);
+        expect(() => new ImmutableMap()).not.toThrow();
     });
 
-    it("should return immutable sets", () => {
-        const set = new Set([1, 2, 3]);
-        const immutableSet = Immutable(set);
-
-        expect(immutableSet).not.toBe(set);
-        expect([...immutableSet]).toEqual([1, 2, 3]);
-        expect(() => {
-            immutableSet.add(4);
-        }).toThrowError(new Error("Cannot call 'add' because it would modify immutable object."));
-        expect(immutableSet.has(1)).toBe(true);
-        expect(immutableSet.has(2)).toBe(true);
-        expect(immutableSet.has(3)).toBe(true);
-        expect(immutableSet.size).toBe(3);
+    it("should instantiate Set without error", () => {
+        const ImmutableSet = immutable(Set);
+        expect(() => new ImmutableSet()).not.toThrow();
     });
 
-    it("should work with Date objects", () => {
-        const date = new Date();
-        const immutableDate = Immutable(date);
-        expect(immutableDate).toBeInstanceOf(Date);
-        expect(immutableDate).not.toBe(date);
-        expect(immutableDate.getTime()).toBe(date.getTime());
+    it("should instantiate Date without error", () => {
+        const ImmutableDate = immutable(Date);
+        expect(() => new ImmutableDate()).not.toThrow();
     });
 
-    it("should work with RegExp objects", () => {
-        const regexp = /test/;
-        const immutableRegexp = Immutable(regexp);
-        expect(immutableRegexp).toBeInstanceOf(RegExp);
-        expect(immutableRegexp).not.toBe(regexp);
-        expect(immutableRegexp.source).toBe(regexp.source);
+    it("should prevent mutating nested objects", () => {
+        const ImmutableObject = immutable(Object);
+        const obj = new ImmutableObject({ a: { b: 2 } });
+        expect(Object.isFrozen(obj.a)).toBe(true);
+        expect(() => (obj.a.b = 3)).toThrowError();
+        expect(obj.a.b).toBe(2);
     });
 });
