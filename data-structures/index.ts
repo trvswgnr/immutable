e085dc5f4ee2bf943d956c08f408e946f861b62e
Travis Wagner <travis.wagner@umusic.com>
2024-05-24 15:57:36 -0400

    update file name

--- data-structures/index.ts
+++ data-structures/index.ts
@@ -1,7 +1,7 @@
 import { ImmutableArray, type ImmutableArrayConstructor } from "./Array";
 import { ImmutableArrayBuffer, type ImmutableArrayBufferConstructor } from "./ArrayBuffer";
 import { ImmutableBigInt64Array, type ImmutableBigInt64ArrayConstructor } from "./BigInt64Array";
-import { ImmutableBigUint64Array, type ImmutableBigUint64ArrayConstructor } from "./x";
+import { ImmutableBigUint64Array, type ImmutableBigUint64ArrayConstructor } from "./BigUint64Array";
 import { ImmutableDataView, type ImmutableDataViewConstructor } from "./DataView";
 
 export {
